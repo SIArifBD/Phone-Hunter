72b640a1b3178440de8b351113b6834c13709e95
SIArifBD <si.arif.cse@gmail.com>
2022-03-01 03:04:14 +0600

    third commit

--- js/app.js
+++ js/app.js
@@ -1,9 +1,31 @@
 const allPhones = () => {
-    const searchText = document.getElementById('input-search');
+    const searchText = document.getElementById('input-search').value;
 
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
     fetch(url)
         .then(res => res.json())
-        .then(value => console.log(value.data));
+        .then(value => displaySearchPhone(value.data));
 }
-allPhones();
+// allPhones();
+/* Display search phones */
+const displaySearchPhone = (phones) => {
+    console.log(phones);
+    for (const phone of phones) {
+        const parent = document.getElementById('phone-container');
+
+        const div = document.createElement('div');
+        div.innerHTML = `
+            <div class="card">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+                         additional content. This content is a little bit longer.</p>
+                </div>
+                <div class="card-footer">
+                     <a href="#" class="btn btn-primary">Show Details</a>
+                </div>
+             </div>
+        `;
+    }
+}
