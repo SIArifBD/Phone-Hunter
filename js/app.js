bd3ab1b8453bbf8b3b3195e0b8228608635da9aa
SIArifBD <si.arif.cse@gmail.com>
2022-03-01 03:32:58 +0600

    fourth commit

--- js/app.js
+++ js/app.js
@@ -5,27 +5,28 @@ const allPhones = () => {
     fetch(url)
         .then(res => res.json())
         .then(value => displaySearchPhone(value.data));
-}
+};
 // allPhones();
 /* Display search phones */
 const displaySearchPhone = (phones) => {
-    console.log(phones);
+    // console.log(phones);
     for (const phone of phones) {
         const parent = document.getElementById('phone-container');
 
         const div = document.createElement('div');
         div.innerHTML = `
             <div class="card">
-                <img src="..." class="card-img-top" alt="...">
+                <img src="${phone.image}" class="card-img-top rounded w-75 h-100" alt="...">
                 <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
-                         additional content. This content is a little bit longer.</p>
+                    <h3 class="card-title">Brand: ${phone.brand}</h3>
+                    <h4 class="card-title">Phone Name: ${phone.phone_name}</h4>
                 </div>
                 <div class="card-footer">
-                     <a href="#" class="btn btn-primary">Show Details</a>
+                     <button onclick ="details()" class ="btn btn-primary">Show Details</button>
                 </div>
              </div>
         `;
+        parent.appendChild(div);
+        console.log(phone);
     }
-}
+};
