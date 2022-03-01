67311dde014369136981d1f32329d5005380abe2
SIArifBD <si.arif.cse@gmail.com>
2022-03-01 06:54:17 +0600

    fifth commit

--- js/app.js
+++ js/app.js
@@ -1,12 +1,14 @@
 const allPhones = () => {
+    document.getElementById('phone-container').innerHTML = "";
     const searchText = document.getElementById('input-search').value;
 
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
     fetch(url)
         .then(res => res.json())
         .then(value => displaySearchPhone(value.data));
+    document.getElementById('input-search').value = "";
 };
-// allPhones();
+
 /* Display search phones */
 const displaySearchPhone = (phones) => {
     // console.log(phones);
@@ -27,6 +29,33 @@ const displaySearchPhone = (phones) => {
              </div>
         `;
         parent.appendChild(div);
-        console.log(phone);
+        // console.log(phone);
     }
+};
+
+const details = (id) => {
+    const url = `https://openapi.programming-hero.com/api/phone/${id}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(dat => displayDetails(dat.data));
+}
+
+const displayDetails = (infos) => {
+    console.log(infos);
+    /*  for (const info of infos) {
+         const parent = document.getElementById('phone-details-container');
+ 
+         const div = document.createElement('div');
+         div.innerHTML = `
+             <div class="card">
+                 <img src="${info.image}" class="card-img-top rounded w-75 h-100" alt="...">
+                 <div class="card-body">
+                     <h3 class="card-title">Brand: ${info.brand}</h3>
+                     <h4 class="card-title">Phone Name: ${info.name}</h4>
+                 </div>
+              </div>
+         `;
+         parent.appendChild(div);
+         console.log(info);
+     } */
 };
